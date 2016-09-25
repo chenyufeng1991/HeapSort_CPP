70db2de90366854c35235a4d4c6147288b71714a
陈宇峰 <yufengcode@gmail.com>
2016-09-25 10:24:31 +0800

    init

--- HeapSort_C++/main.cpp
+++ HeapSort_C++/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void HeapSort(vector<int> &vec);
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {2,7,1,6,9,0};
+    vector<int>vectorArray(arr, arr + sizeof(arr)/sizeof(int));
+    HeapSort(vectorArray);
+    vector<int>::iterator vectorIte;
+    for (vectorIte = vectorArray.begin(); vectorIte != vectorArray.end(); vectorIte++)
+    {
+        cout << *vectorIte << " ";
+    }
+
     return 0;
 }
+
+void HeapSort(vector<int> &vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        make_heap(vec.begin(), vec.end() - i, less<int>()); // 建立小顶堆,输出递增序列；
+        swap(vec[0], vec[vec.size() - 1 - i]); //
+    }
+}
+
